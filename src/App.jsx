6bb741a36fc50a4b470d1f55221013bd353451f8
pritesh <priteshbarot1896@gmail.com>
2023-03-15 19:11:02 +0530

    picture added

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { Navbar } from "../src/components/Navbar";
 import { Hero_section } from "../src/components/hero_section";
 import { About } from "../src/components/about";
 import { Contact } from "../src/components/contact";
+import { Work } from "../src/components/work";
+
+
 function App() {
   return (
     <div className="contanier">
         <Navbar />
         <Hero_section />
         <About />
+        <Work />
         <Contact />
     </div>
   );
